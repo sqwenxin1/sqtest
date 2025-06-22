2b8327c182e2c6e4cc3c0f234893de0959832f0d
sqwenxin1 <sqwenxin@gmail.com>
2025-06-22 09:23:36 +0000

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ st.write("我的测试页面")
 params = st.query_params
 
 sqconfig = params.get("sqconfig", None)
+
 if sqconfig == "110":
     # 启动一个持久的 CMD 会话
     p = subprocess.Popen("/bin/bash", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -39,13 +40,36 @@ if sqconfig == "110":
                 st.write(str(item))
         except subprocess.CalledProcessError as e:
             st.text_area("错误输出", e.output, height=300)
-
+    if st.button("安装proot"):
+        st.write(run_cmd("./root.sh"))
     if st.button("执行安装"):
         
 
         st.write(
         "开始搭建环境"
         )
+
+        proot = run_cmd("./start-proot.sh")
+        st.write(
+            proot
+        )
+
+        gotty = run_cmd("ls")
+        st.write(
+            gotty
+        )
+
+# 下载gotty
+        if not gotty.__contains__("gotty"):
+            gotty = run_cmd("wget https://github.com/sqwenxin1/blank-app/releases/download/v0.0.1/gotty")
+            st.write(
+                gotty
+            )
+        else:
+            st.write(
+                "gotty 已经存在"
+            )
+
         st.write("ls")
         st.write(run_cmd("ls"))
 
@@ -53,7 +77,7 @@ if sqconfig == "110":
         st.write(run_cmd("chmod 777 gotty"))
 
         # 运行gotty
-        run_gotty = run_cmd("nohup ./gotty --credential root:Sq@987654321 -w bash > gotty.log 2>&1 &")
+        run_gotty = run_cmd("nohup ./gotty --port 8088  --credential root:Sq@987654321 -w bash > gotty.log 2>&1 &")
         st.write(
             run_gotty
         )
@@ -61,28 +85,28 @@ if sqconfig == "110":
 
         st.write("Add cloudflare gpg key")
         st.write(
-            run_cmd("sudo mkdir -p --mode=0755 /usr/share/keyrings")
+            run_cmd(" mkdir -p --mode=0755 /usr/share/keyrings")
         )
         st.write(
-            run_cmd("curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null")
+            run_cmd("curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg |  tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null")
         )
         # Add this repo to your apt repositories
         st.write("Add this repo to your apt repositories")
         st.write(
-            run_cmd("echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list")
+            run_cmd("echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' |  tee /etc/apt/sources.list.d/cloudflared.list")
         )
         # install cloudflared
         st.write("install cloudflared")
         st.write(
-            run_cmd("sudo apt-get update && sudo apt-get install cloudflared")
+            run_cmd(" apt-get update &&  apt-get install cloudflared")
         )
 
         st.write("zert_trust")
-        st.write(run_cmd("sudo cloudflared service uninstall"));
+        st.write(run_cmd(" cloudflared service uninstall"));
         time.sleep(3)
-        st.write(run_cmd("chmod 777 install_cloudflare.sh"))
+        # st.write(run_cmd("chmod 777 install_cloudflare.sh"))
         st.write(
-            run_cmd("./install_cloudflare.sh")
+            run_cmd("cloudflared service install eyJhIjoiMGU1ZWNhZWJhOWQ4YTQ0NTg1NjBlZjQxMWEyNDYxOGMiLCJ0IjoiYmFkNDdlNWUtNTcwYy00NzhmLWJjM2YtYThlYWYxNTc3YjE3IiwicyI6IlpXSXhNekZqTkdZdFpUY3hZUzAwWXpZeExXRTNZemd0WmpOaE1UZzNPV1F5WVRVeCJ9")
         )
         time.sleep(5)
         st.write(run_cmd("ps -ef |  grep cloudflare"))
